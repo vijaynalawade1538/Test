a8cf538a6d9007dff9a5f5a212a4bf7f2a6389ff
Sangram <Sangram@DESKTOP-7U3RHQE>
2021-04-16 17:26:21 +0530

    1. Creating project 2. @ResponseBody annotation 3. JSON REST API annotations 4.@PathVariable annotation

---
+++ RestSpringMVC/src/main/java/com/gontuseries/restspringmvcapplication/model/Student.java
@@ -0,0 +1,63 @@
+package com.gontuseries.restspringmvcapplication.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"student_name","studentDOB","studentHobby","studentSkills","studentAddress"})
+public class Student {
+	
+	@JsonProperty("student_name")
+	private String studentName;
+	private String studentHobby;
+	//@JsonIgnore
+	private long studentMobile;
+	private Date studentDOB;
+	private ArrayList<String> studentSkills;
+	private Address studentAddress;
+	
+	
+	public Address getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(Address studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	public long getStudentMobile() {
+		return studentMobile;
+	}
+	public void setStudentMobile(long studentMobile) {
+		this.studentMobile = studentMobile;
+	}
+	public Date getStudentDOB() {
+		return studentDOB;
+	}
+	public void setStudentDOB(Date studentDOB) {
+		this.studentDOB = studentDOB;
+	}
+	public ArrayList<String> getStudentSkills() {
+		return studentSkills;
+	}
+	public void setStudentSkills(ArrayList<String> studentSkills) {
+		this.studentSkills = studentSkills;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String name) {
+		this.studentName = name;
+	}
+	public String getStudentHobby() {
+		return studentHobby;
+	}
+	public void setStudentHobby(String studentHobby) {
+		this.studentHobby = studentHobby;
+	}
+
+}
